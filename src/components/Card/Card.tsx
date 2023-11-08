0cbc8fb27bc99607f3d9eb2d4329fd8e2aaeb66c
Patrick Narciso <patrick.ncsilva@gmail.com>
2023-11-07 21:30:37 -0300

    feat: finaliza componente Card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,12 +1,52 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
+import {
+  FiEdit as Edit,
+  FiSave as Save,
+  FiXCircle as Cancel,
+  FiTrash as Delete,
+} from 'react-icons/fi';
 import { CardProps } from './Card.types';
-import { Container, Description, Title } from './Card.styles';
+import { Container, Description, Title, Row, Column } from './Card.styles';
 
-const Card: FunctionComponent<CardProps> = ({ title, description }) => {
+const Card: FunctionComponent<CardProps> = ({
+  title,
+  description,
+  onChangeTitle,
+  onChangeDescription,
+  handleSave,
+  handleDelete,
+}) => {
+  const [isEditMode, setEditMode] = useState<boolean>(false);
+
   return (
     <Container>
-      <Title>{title}</Title>
-      <Description>{description}</Description>
+      <Row>
+        <Title
+          readOnly={!isEditMode}
+          placeholder="Your title here..."
+          value={title}
+          onChange={onChangeTitle}
+        />
+        {!isEditMode && <Edit onClick={() => setEditMode(true)} />}
+      </Row>
+      <Description
+        readOnly={!isEditMode}
+        placeholder="Your description here..."
+        value={description}
+        onChange={onChangeDescription}
+      />
+      <Row>
+        {isEditMode ? (
+          <>
+            <Cancel onClick={() => setEditMode(false)} />
+            <Save onClick={handleSave} />
+          </>
+        ) : (
+          <Column>
+            <Delete onClick={handleDelete} />
+          </Column>
+        )}
+      </Row>
     </Container>
   );
 };
--- src/components/Card/Card.types.ts
+++ src/components/Card/Card.types.ts
@@ -1,4 +1,11 @@
+import { Card } from '@/entities';
+import { ChangeEvent } from 'react';
+
 export type CardProps = {
   title: string;
   description: string;
+  onChangeTitle: (event: ChangeEvent<HTMLInputElement>) => void;
+  onChangeDescription: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+  handleSave: (title: string, description: string, list: string) => Card;
+  handleDelete: (id: number) => Card[];
 };
