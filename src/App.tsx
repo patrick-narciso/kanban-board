b02b568627c0452dd0343dfd5fcd2884b0de5724
Patrick Narciso <patrick.ncsilva@gmail.com>
2023-11-08 22:14:01 -0300

    feat: cria pagina kanban board

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { useState } from 'react';
-import { Card } from './components';
-
-interface CardContent {
-  [x: string]: string;
-}
+import { BoardCardsProvider } from './features/Board/contexts/board-cards';
+import KanbanBoard from './pages/KanbanBoard';
 
 function App() {
-  const [value, setValue] = useState<CardContent>({ title: '', description: '' });
-
-  const handleTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue((prev) => ({ ...prev, title: event.target.value }));
-  };
-
-  const handleDescription = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setValue((prev) => ({ ...prev, description: event.target.value }));
-  };
-
   return (
-    <>
-      <Card
-        title={value.title}
-        description={value.description}
-        onChangeTitle={handleTitle}
-        onChangeDescription={handleDescription}
-      />
-    </>
+    <BoardCardsProvider>
+      <KanbanBoard />
+    </BoardCardsProvider>
   );
 }
 
---
+++ src/pages/KanbanBoard.tsx
@@ -0,0 +1,7 @@
+import { ListsContainer } from '@/features/Board/layouts/lists-container/lists-container';
+
+function KanbanBoard() {
+  return <ListsContainer />;
+}
+
+export default KanbanBoard;
